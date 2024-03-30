8349b7198cc7035ea484a3cd5180c4145290b615
Aamir Ansari <amirbxr2003@gmail.com>
2024-03-31 05:19:08 +0530

    hypertune

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -13,6 +13,7 @@ from sklearn.model_selection import RandomizedSearchCV
 from catboost import CatBoostRegressor
 from xgboost import XGBRegressor
 from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 
 import os 
 import sys 
@@ -42,19 +43,58 @@ class ModelTrainer:
             )
 
             models = {
-                "LinearRegression":LinearRegression(),
-                "Lasso":Lasso(),
-                "Ridge":Ridge(),
-                "KNeighbors Regressor":KNeighborsRegressor(),
                 "Decision Tree":DecisionTreeRegressor(),
                 "Random Forest Regressor":RandomForestRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "LinearRegression":LinearRegression(),
                 "XGBRegressor":XGBRegressor(),
                 "CatBoost Regressor":CatBoostRegressor(),
                 "AdaBoost Regressor":AdaBoostRegressor(),
             }
+            
+
+
+            params={
+                "Decision Tree": {
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
 
             model_report:dict= evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,
-                                              y_test=y_test,models=models )
+                                              y_test=y_test,models=models,param=params )
             
 
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import dill
 import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -22,13 +23,19 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
      try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            
+            para = param[list(param.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
