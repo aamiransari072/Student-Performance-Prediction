2fd9c4c10928c2874faaaab2764dc0236486975f
Aamir Ansari <amirbxr2003@gmail.com>
2024-04-01 07:29:00 +0530

    integrated

--- src/utils.py
+++ src/utils.py
@@ -56,4 +56,12 @@ def evaluate_models(x_train,y_train,x_test,y_test,models,param):
          raise CustomException(e, sys)
 
 
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
         
